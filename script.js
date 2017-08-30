7b85db19ed06c4355ba0ccb2258e5e91063acaf6
Konstantin Korobkov <konstantin.korobkov@waveaccess.ru>
2017-08-30 14:34:16 +0300

    add validation and design

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-GITHUB_FOLDER_LINK = "https://raw.githubusercontent.com/korobkov-k/yandex-njs-school-test/master/json";
+GITHUB_FOLDER_LINK = "https://gitcdn.link/repo/korobkov-k/yandex-njs-school-test/master/json";
 MyForm = {
     validate: function () {
         var formData = this.getData();
-        console.log(formData);
+        var errorFields = [];
+
+        //Check name
+        if (formData.fio.split(" ").length !== 3) {
+            errorFields.push("fio")
+        }
+
+        //Check email
+        if (!(formData.email.indexOf("@") > -1 &&
+              formData.email.split("@")[0].length > 0 &&
+              ["ya.ru", "yandex.ru", "yandex.ua", "yandex.by", "yandex.kz", "yandex.com"].indexOf(formData.email.split("@")[1]) > -1)) {
+            errorFields.push("email")
+        }
+
+        //Check phone
+        var maxSum = 30;
+        if (formData.phone.match(/\+7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}/g)[0] == formData.phone) {
+            var numbers = formData.phone.match(/[0-9]/g);
+            var sum = numbers.reduce(function(a, b) {
+                return parseInt(a) + parseInt(b);
+            });
+            if (sum > maxSum) {
+                errorFields.push("phone")
+            }
+        } else {
+            errorFields.push("phone")
+        }
+
         return {
-            isValid: true
+            isValid: errorFields.length === 0,
+            errorFields: errorFields
         }
     },
     getData: function () {
@@ -15,11 +43,20 @@ MyForm = {
             phone: form.phone.value
         }
     },
-    setData: function () {
-        // body...
+    setData: function (data) {
+        var form = document.getElementById('myForm');
+        if (data.fio != null) form.fio.value = data.fio;
+        if (data.email != null) form.email.value = data.email;
+        if (data.phone != null) form.phone.value = data.phone;
     },
     submit: function () {
         var validationResult = MyForm.validate();
+
+        var form = document.forms['myForm'];
+        var inputs = form.getElementsByTagName('input');
+        for (var i in inputs) {
+            inputs[i].className = "";
+        }
 
         if (validationResult.isValid) {
             var percent = Math.random();
@@ -37,24 +74,41 @@ MyForm = {
 
             xhr.send();
             resultContainer.innerHTML = "";
+            resultContainer.className = "";
             button.value = 'Идет загрузка...';
             button.disabled = true;
             xhr.onreadystatechange = function () {
                 if (xhr.readyState !== 4) return;
 
-                button.value = 'Отправить';
-                button.disabled = false;
-
-
                 var responseObj = JSON.parse(xhr.response);
                 if (xhr.status !== 200) {
                     resultContainer.innerHTML = responseObj.status + "<span class='reason'>" + responseObj.reason + "</span>";
+                    resultContainer.className = "error";
+                    button.value = 'Отправить';
+                    button.disabled = false;
                 } else {
                     resultContainer.innerHTML = responseObj.status;
+                    if (responseObj.status === "progress" && typeof responseObj.timeout === "number") {
+                        resultContainer.className = "progress";
+                        window.setTimeout(function () {
+                            MyForm.submit();
+                        }, responseObj.timeout)
+                    } else if (responseObj.status === "error") {
+                        resultContainer.className = "error";
+                        resultContainer.innerHTML = responseObj.status + "<span class='reason'>" + responseObj.reason + "</span>";
+                        button.value = 'Отправить';
+                        button.disabled = false;
+                    } else {
+                        resultContainer.className = "success";
+                        button.value = 'Отправить';
+                        button.disabled = false;
+                    }
                 }
             }
         } else {
-
+            validationResult.errorFields.forEach(function (name) {
+                form[name].className = "error";
+            })
         }
 
         return false;
